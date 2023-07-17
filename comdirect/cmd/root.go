jsattler/go-comdirect

commit 515c1135a8d809383b39e4eb6a281bd023f60092

feat: add a date filter for transactions (#19)
* Add a date filter for transactions

With this filter a --since flag is added to the transaction command
which takes a date as input. The API is queried until the date is part
of the results.

* Ignore NOTBOOKED transactions on date filtering

* Respect comdirect API rate limit of 10 requests/s

---------

Co-authored-by: Joshua Sattler <34030048+jsattler@users.noreply.github.com>

comdirect/cmd/root.go
@@ -3,13 +3,14 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"github.com/jsattler/go-comdirect/comdirect/keychain"
-	"github.com/jsattler/go-comdirect/pkg/comdirect"
-	"github.com/spf13/cobra"
 	"log"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/jsattler/go-comdirect/comdirect/keychain"
+	"github.com/jsattler/go-comdirect/pkg/comdirect"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -19,6 +20,7 @@ var (
 	formatFlag       string
 	indexFlag        string
 	countFlag        string
+	sinceFlag        string
 	downloadFlag     bool
 	usernameFlag     string
 	passwordFlag     string
@@ -44,6 +46,8 @@ func init() {
 
 	documentCmd.Flags().StringVar(&folderFlag, "folder", "", "folder to save downloads")
 	documentCmd.Flags().BoolVar(&downloadFlag, "download", false, "whether to download documents")
+
+	transactionCmd.PersistentFlags().StringVar(&sinceFlag, "since", time.Now().Add(time.Hour*-1*24*30).Format("2006-01-02"), "Date of the earliest transaction date to retrieve in the form YYYY-MM-DD")
 
 	rootCmd.PersistentFlags().StringVar(&indexFlag, "index", "0", "page index")
 	rootCmd.PersistentFlags().StringVar(&countFlag, "count", "20", "page count")

comdirect/cmd/transaction.go
@@ -4,11 +4,14 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
 	"github.com/jsattler/go-comdirect/pkg/comdirect"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
-	"log"
-	"os"
 )
 
 var (
@@ -22,16 +25,49 @@ var (
 )
 
 func transaction(cmd *cobra.Command, args []string) {
-	client := initClient()
-	ctx, cancel := contextWithTimeout()
-	defer cancel()
-
-	options := comdirect.EmptyOptions()
-	options.Add(comdirect.PagingCountQueryKey, countFlag)
-	options.Add(comdirect.PagingFirstQueryKey, indexFlag)
-	transactions, err := client.Transactions(ctx, args[0], options)
+	const dateLayout = "2006-01-02"
+	since, err := time.Parse(dateLayout, sinceFlag)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to parse date from command line: %s", err)
+	}
+
+	client := initClient()
+	var transactions = &comdirect.AccountTransactions{}
+
+	page := 1
+	pageCount, err := strconv.Atoi(countFlag)
+	if err != nil {
+		log.Fatalf("Can't convert string to int: %s", err)
+	}
+	for {
+		options := comdirect.EmptyOptions()
+		options.Add(comdirect.PagingCountQueryKey, fmt.Sprint(pageCount*page))
+		options.Add(comdirect.PagingFirstQueryKey, fmt.Sprint(0))
+		ctx, cancel := contextWithTimeout()
+		defer cancel()
+
+		transactions, err = client.Transactions(ctx, args[0], options)
+		if err != nil {
+			log.Fatalf("Error retrieving transactions: %e", err)
+		}
+
+		if len(transactions.Values) == 0 {
+			break
+		}
+
+		lastDate, err := time.Parse(dateLayout, transactions.Values[len(transactions.Values)-1].BookingDate)
+		if err != nil {
+			log.Fatalf("Failed to parse date from command line: %s", err)
+		}
+		if transactions.Paging.Matches == len(transactions.Values) || lastDate.Before(since) {
+			break
+		}
+		page++
+	}
+
+	transactions, err = transactions.FilterSince(since)
+	if err != nil {
+		log.Fatalf("Error filtering transactions by date: %e", err)
 	}
 
 	switch formatFlag {

go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/olekukonko/tablewriter v0.0.5
+	golang.org/x/time v0.3.0
 	github.com/spf13/cobra v1.7.0
 	github.com/zalando/go-keyring v0.2.3
 	golang.org/x/term v0.10.0

pkg/comdirect/account.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type AccountBalance struct {
@@ -136,4 +137,24 @@ func (c *Client) Transactions(ctx context.Context, accountId string, options ...
 	_, err = c.http.exchange(req, tr)
 
 	return tr, err
+}
+
+func (a *AccountTransactions) FilterSince(date time.Time) (*AccountTransactions, error) {
+	filteredTransactions := &AccountTransactions{Paging: a.Paging}
+	for _, v := range a.Values {
+		if v.BookingStatus == "NOTBOOKED" {
+			continue
+		}
+		const transactionDateLayout = "2006-01-02"
+		d, err := time.Parse(transactionDateLayout, v.BookingDate)
+		if err != nil {
+			log.Fatalf("Failed to parse date from transaction: %s", err)
+		}
+		if d.Before(date) {
+			break
+		} else {
+			filteredTransactions.Values = append(filteredTransactions.Values, v)
+		}
+	}
+	return filteredTransactions, nil
 }

pkg/comdirect/account_test.go
@@ -53,15 +53,16 @@ func TestClient_Transactions(t *testing.T) {
 	client := clientFromEnv()
 	ctx, cancel := contextTimeout10Seconds()
 	defer cancel()
-	transactions, err := client.Transactions(ctx, os.Getenv("COMDIRECT_ACCOUNT_ID"))
+	since := time.Now().Add(time.Hour * -1 * 24 * 30)
+	transactions, err := client.Transactions(ctx, os.Getenv("COMDIRECT_ACCOUNT_ID"), since)
 
 	if err != nil {
 		t.Errorf("failed to exchange account balance %s", err)
 		return
 	}
 
-	for _, t := range transactions {
-		fmt.Printf("%v\n", t)
+	for _, tr := range transactions.Values {
+		fmt.Printf("%v\n", tr)
 	}
 }
 

pkg/comdirect/auth.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/jsattler/go-comdirect/internal/mediatype"
+	"golang.org/x/time/rate"
 )
 
 // Authenticator is responsible for authenticating against the comdirect REST API.
@@ -101,7 +102,7 @@ type authStatus struct {
 func NewAuthenticator(options *AuthOptions) *Authenticator {
 	return &Authenticator{
 		authOptions: options,
-		http:        &HTTPClient{http.Client{Timeout: DefaultHttpTimeout}},
+		http:        &HTTPClient{http.Client{Timeout: DefaultHttpTimeout}, *rate.NewLimiter(10, 10)},
 	}
 }
 

pkg/comdirect/client.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"net/http"
 	"time"
+
+	"golang.org/x/time/rate"
 )
 
 const (
@@ -94,7 +96,7 @@ func (o *Options) Values() Values {
 func NewWithAuthenticator(authenticator *Authenticator) *Client {
 	return &Client{
 		authenticator: authenticator,
-		http:          &HTTPClient{http.Client{Timeout: DefaultHttpTimeout}},
+		http:          &HTTPClient{http.Client{Timeout: DefaultHttpTimeout}, *rate.NewLimiter(10, 10)},
 	}
 }
 
@@ -106,7 +108,7 @@ func NewWithAuthOptions(options *AuthOptions) *Client {
 func NewWithAuthentication(authentication *Authentication) *Client {
 	return &Client{
 		authentication: authentication,
-		http:           &HTTPClient{http.Client{Timeout: DefaultHttpTimeout}},
+		http:           &HTTPClient{http.Client{Timeout: DefaultHttpTimeout}, *rate.NewLimiter(10, 10)},
 	}
 }
 

pkg/comdirect/util.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"net/url"
 	"time"
+
+	"golang.org/x/time/rate"
 )
 
 // TODO: Think about where to put the stuff in here.
@@ -15,6 +17,7 @@ import (
 
 type HTTPClient struct {
 	http.Client
+	rate.Limiter
 }
 
 // api.comdirect.de/api/{path}
@@ -55,6 +58,10 @@ func generateRequestID() string {
 }
 
 func (h *HTTPClient) exchange(request *http.Request, target interface{}) (*http.Response, error) {
+	err := h.Wait(request.Context())
+	if err != nil {
+		return nil, err
+	}
 	res, err := h.Do(request)
 	if err != nil {
 		return res, err

commit 8dfc4e5373ddbc8c48c17883743b425968de5250

fix: paging on transactions
It was broken in commit 515c113, because the count parameter was ignored
with the new since flag. Now the since flag is empty per default and
only if the parameter is set the transactions are filtered. Otherwise
the count parameter is used.

Refs: #24

comdirect/cmd/root.go
@@ -47,7 +47,7 @@ func init() {
 	documentCmd.Flags().StringVar(&folderFlag, "folder", "", "folder to save downloads")
 	documentCmd.Flags().BoolVar(&downloadFlag, "download", false, "whether to download documents")
 
-	transactionCmd.PersistentFlags().StringVar(&sinceFlag, "since", time.Now().Add(time.Hour*-1*24*30).Format("2006-01-02"), "Date of the earliest transaction date to retrieve in the form YYYY-MM-DD")
+	transactionCmd.PersistentFlags().StringVar(&sinceFlag, "since", "", "Date of the earliest transaction date to retrieve in the form YYYY-MM-DD")
 
 	rootCmd.PersistentFlags().StringVar(&indexFlag, "index", "0", "page index")
 	rootCmd.PersistentFlags().StringVar(&countFlag, "count", "20", "page count")

comdirect/cmd/transaction.go
@@ -25,49 +25,22 @@ var (
 )
 
 func transaction(cmd *cobra.Command, args []string) {
-	const dateLayout = "2006-01-02"
-	since, err := time.Parse(dateLayout, sinceFlag)
-	if err != nil {
-		log.Fatalf("Failed to parse date from command line: %s", err)
-	}
-
-	client := initClient()
 	var transactions = &comdirect.AccountTransactions{}
+	var err error
+	client := initClient()
+	ctx, cancel := contextWithTimeout()
+	defer cancel()
 
-	page := 1
-	pageCount, err := strconv.Atoi(countFlag)
-	if err != nil {
-		log.Fatalf("Can't convert string to int: %s", err)
-	}
-	for {
+	if sinceFlag == "" {
 		options := comdirect.EmptyOptions()
-		options.Add(comdirect.PagingCountQueryKey, fmt.Sprint(pageCount*page))
-		options.Add(comdirect.PagingFirstQueryKey, fmt.Sprint(0))
-		ctx, cancel := contextWithTimeout()
-		defer cancel()
-
+		options.Add(comdirect.PagingCountQueryKey, countFlag)
+		options.Add(comdirect.PagingFirstQueryKey, indexFlag)
 		transactions, err = client.Transactions(ctx, args[0], options)
 		if err != nil {
-			log.Fatalf("Error retrieving transactions: %e", err)
+			log.Fatalf("Failed to retrieve transactions: %s", err)
 		}
-
-		if len(transactions.Values) == 0 {
-			break
-		}
-
-		lastDate, err := time.Parse(dateLayout, transactions.Values[len(transactions.Values)-1].BookingDate)
-		if err != nil {
-			log.Fatalf("Failed to parse date from command line: %s", err)
-		}
-		if transactions.Paging.Matches == len(transactions.Values) || lastDate.Before(since) {
-			break
-		}
-		page++
-	}
-
-	transactions, err = transactions.FilterSince(since)
-	if err != nil {
-		log.Fatalf("Error filtering transactions by date: %e", err)
+	} else {
+		transactions = getTransactionsSince(sinceFlag, client, args[0])
 	}
 
 	switch formatFlag {
@@ -80,6 +53,54 @@ func transaction(cmd *cobra.Command, args []string) {
 	default:
 		printTransactionTable(transactions)
 	}
+}
+
+func getTransactionsSince(since string, client *comdirect.Client, accountID string) *comdirect.AccountTransactions {
+	const dateLayout = "2006-01-02"
+	var transactions = &comdirect.AccountTransactions{}
+	ctx, cancel := contextWithTimeout()
+	defer cancel()
+
+	s, err := time.Parse(dateLayout, since)
+	if err != nil {
+		log.Fatalf("Failed to parse date from command line: %s", err)
+	}
+
+	page := 1
+	pageCount, err := strconv.Atoi(countFlag)
+	if err != nil {
+		log.Fatalf("Can't convert string to int: %s", err)
+	}
+	for {
+		options := comdirect.EmptyOptions()
+		options.Add(comdirect.PagingCountQueryKey, fmt.Sprint(pageCount*page))
+		options.Add(comdirect.PagingFirstQueryKey, fmt.Sprint(0))
+
+		transactions, err = client.Transactions(ctx, accountID, options)
+		if err != nil {
+			log.Fatalf("Error retrieving transactions: %e", err)
+		}
+
+		if len(transactions.Values) == 0 {
+			break
+		}
+
+		lastDate, err := time.Parse(dateLayout, transactions.Values[len(transactions.Values)-1].BookingDate)
+		if err != nil {
+			lastDate = time.Now()
+
+		}
+		if transactions.Paging.Matches == len(transactions.Values) || lastDate.Before(s) {
+			break
+		}
+		page++
+	}
+
+	transactions, err = transactions.FilterSince(s)
+	if err != nil {
+		log.Fatalf("Error filtering transactions by date: %e", err)
+	}
+	return transactions
 }
 
 func printJSON(v interface{}) {